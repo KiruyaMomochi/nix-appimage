0df33127bc215572ef626f91b09cda763adea773
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-07-23 15:55:17 +0800

    deps: update dependencies

--- flake.nix
+++ flake.nix
@@ -2,25 +2,11 @@
   description = "A basic AppImage bundler";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-
-    appimage-runtime = {
-      url = "github:AppImageCrafters/appimage-runtime";
-      flake = false;
-    };
-
-    squashfuse = {
-      # * 53e1b97002c7bc13392a6b0121680d28a94a9715 add support for mounting of subdirectory
-      #   which changes API of sqfs_open_image and sqfs_usage
-      #   The correct patch should be add NULL when calling them in appimage-runtime
-      # * 1d6da796e7d4447af8ce2d7efe51dc053d314221 add low-level uid=N and gid=N options
-      url = "github:vasi/squashfuse/0.1.105";
       flake = false;
     };
   };
